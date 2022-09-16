d539290ad74128f32b311068f9391ec576ed97f4
Ndeta Innocent <you@example.com>
2022-09-16 13:19:24 +0300

    Id element could be name to make code cleaner

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 //Function to to take, hold and print the end product of the calculation where the input id is 'inputwindow'
+const inputwindow=document.getElementById('inputwindow')
+
 function takeValue(x) {
-    document.getElementById('inputwindow').value += x;
+    inputwindow.value += x;
 }
 
 function clearInput(y) {
-    document.getElementById('inputwindow').value = y;
+    inputwindow.value = y;
 }
 
 function calculateResult() {
-    var result = eval(document.getElementById('inputwindow').value);
-    document.getElementById('inputwindow').value = result;
+    const result = eval(inputwindow.value);
+    inputwindow.value = result;
 }
